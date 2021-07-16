b49c6a8bd856690cf250b2d1bcbd5f34eb08342f
myhacktech <86517958+myhacktech@users.noreply.github.com>
2021-07-16 21:48:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,7 @@
-
 <html>
-<body><center>
-<h1><font face="algerian">vihar tourism</FONT>
-</CENTER>"<?php
-
-{
-$n=$_POST['uname'];
-$s=$_POST['psw'];
-$con=new mysqli('localhost','root','sunbeam','mmtech');
-$q="select * from login where name='$n' and password='$s'";
-$record=$con->query($q);
-
-
-
-	 while($row=$record->fetch_assoc())
-  {
-	if($row['password']==$s)
-	echo "hello mr.$n welcome to your dashboard
-
-";
-
-	else 
-		echo 'error';
-  }
-
- 
-$con->close();
-}
+<body>
+<?php
+echo "hello,please ";
 ?>
 </body>
 </html>
